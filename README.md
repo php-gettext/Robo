d3af03ed11a51a4eedbe3e997eafed0a91f414f9
oscarotero <oom@oscarotero.com>
2015-10-02 01:00:30 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# robo-gettext
-robo task to extract gettext values from files
+# GettextRobo
+
+[Robo](http://robo.li) task to extract gettext values from files using [gettext/gettext](https://github.com/oscarotero/Gettext) library
+
+Created by Oscar Otero <http://oscarotero.com> <oom@oscarotero.com> (MIT License)
+
+## Install
+
+Using composer:
+
+```bash
+composer require gettext/robo
+```
+
+## usage example
+
+Create a RoboFile.php with the following code:
+
+```php
+require 'vendor/autoload.php';
+
+class RoboFile extends \Robo\Tasks
+{
+	use Gettext\Robo\GettextScanner;
+
+    /**
+     * Scan files to find new gettext values
+     */
+    public function gettext()
+    {
+    	$this->taskGettextScanner()
+    		->extract(__DIR__.'/templates/')
+    		->extract(__DIR__.'/js/', '/.*\.js/') //directory + regex
+    		->generate(__DIR__.'/Locale/gl/LC_MESSAGES/messages.mo')
+    		->generate(__DIR__.'/Locale/es/LC_MESSAGES/messages.mo')
+    		->generate(__DIR__.'/Locale/en/LC_MESSAGES/messages.mo')
+    		->run();
+    }
+}
+```
+
+Use robo to run the code:
+
+```bash
+robo gettext
+```
---
+++ src/GettextScanner.php
@@ -0,0 +1,148 @@
+<?php
+namespace Gettext\Robo;
+
+use Gettext\Translations;
+use FilesystemIterator;
+use MultipleIterator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RecursiveRegexIterator;
+use RegexIterator;
+use Robo\Task\BaseTask;
+use Robo\Contract\TaskInterface;
+
+/**
+ * Trait to scan files to get gettext entries
+ */
+trait GettextScanner
+{
+    /**
+     * Init a gettext task
+     */
+    protected function taskGettextScanner()
+    {
+        return new TaskGettextScanner();
+    }
+}
+
+class TaskGettextScanner extends BaseTask implements TaskInterface
+{
+    protected $iterator;
+    protected $targets = [];
+
+    public function __construct()
+    {
+        $this->iterator = new MultipleIterator(MultipleIterator::MIT_NEED_ANY);
+    }
+
+    /**
+     * Add a new source folder
+     *
+     * @param string      $path
+     * @param null|string $regex
+     *
+     * @return $this
+     */
+    public function extract($path, $regex = null)
+    {
+        $directory = new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS);
+        $iterator = new RecursiveIteratorIterator($directory);
+
+        if ($regex) {
+            $iterator = new RegexIterator($iterator, $regex, RecursiveRegexIterator::GET_MATCH);
+        }
+
+        $this->iterator->attachIterator($iterator);
+
+        return $this;
+    }
+
+    /**
+     * Add a new target
+     *
+     * @param string $path
+     *
+     * @return $this
+     */
+    public function generate($path)
+    {
+        $this->targets[] = $path;
+
+        return $this;
+    }
+
+    /**
+     * Run the task
+     */
+    public function run()
+    {
+        foreach ($this->targets as $target) {
+            $translations = new Translations();
+
+            $this->scan($translations);
+
+            if (is_file($target)) {
+                $fn = $this->getFunctionName('from', $target, 'File');
+
+                $translations->mergeWith(Translations::$fn($target), Translations::MERGE_HEADERS | Translations::MERGE_LANGUAGE | Translations::MERGE_PLURAL | Translations::MERGE_COMMENTS);
+            }
+
+            $fn = $this->getFunctionName('to', $target, 'File');
+            $dir = dirname($target);
+
+            if (!is_dir($dir)) {
+                mkdir($dir, 0777, true);
+            }
+
+            $translations->$fn($target);
+
+            $this->printTaskInfo("Gettext exported to {$target}");
+        }
+    }
+
+    /**
+     * Execute the scan
+     *
+     * @param Translations $translations
+     */
+    private function scan(Translations $translations)
+    {
+        foreach ($this->iterator as $each) {
+            foreach ($each as $file) {
+                if ($file === null || !$file->isFile()) {
+                    continue;
+                }
+
+                $target = $file->getPathname();
+
+                if (($fn = $this->getFunctionName('addFrom', $target, 'File'))) {
+                    $translations->$fn($target);
+                }
+            }
+        }
+    }
+
+    /**
+     * Get the format based in the extension
+     *
+     * @param string $file
+     *
+     * @return string|null
+     */
+    private function getFunctionName($prefix, $file, $suffix)
+    {
+        switch (pathinfo($file, PATHINFO_EXTENSION)) {
+            case 'php':
+                return "{$prefix}PhpCode{$suffix}";
+
+            case 'js':
+                return "{$prefix}JsCode{$suffix}";
+
+            case 'po':
+                return "{$prefix}Po{$suffix}";
+
+            case 'mo':
+                return "{$prefix}Mo{$suffix}";
+        }
+    }
+}
